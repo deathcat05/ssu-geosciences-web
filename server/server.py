742a2be777fb508e5d7b1c5dbd4c6d22fa35439f
Catherine Meyer <c93@catherinemeyer.org>
2019-03-06 21:50:44 -0800

    UI cleanup

--- server/server.py
+++ server/server.py
@@ -6,13 +6,16 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def hello_world():
         return 'Hello, world!'
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -31,5 +34,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(filename)
             return 'file uploaded successfully!'
+
+
 if __name__ == '__main__':
     app.run()
